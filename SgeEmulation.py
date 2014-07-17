9a52025b04dab9edcd5bac78258dd77fd40cbe03
moink <theresa.robinson@gmail.com>
2014-07-17 20:03:03 +0200

    Added another test, that you can only add a job in a queued or hold state Refactored a touch Still isn't passing test_job_runs

--- SgeEmulation.py
+++ SgeEmulation.py
@@ -1,12 +1,17 @@
 import unittest
 import random
+
+class SgeError(Exception):
+    """Base class for exceptions in this module."""
+    pass
  
 class SgeJob():
  
-    def __init__(self,jobname,runtime=100):
-        self._jobname=jobname
-        self._runtime=runtime
-        self._status="queued"
+    def __init__(self,jobname,runtime=100,initial_status="queued"):
+        self.jobname=jobname
+        self.runtime=runtime
+        self.status=initial_status
+        
  
 class SgeEmulator():
 #This is an emulator for a Sun Grid Engine
@@ -22,43 +27,52 @@ class SgeEmulator():
         except ValueError:
             self._slots=min_slots
  
-    def add_job(self,jobname,runtime=100):
-        self._joblist.append(SgeJob(jobname))
+    def add_job(self,jobname,runtime=100,initial_status="queued"):
+        if initial_status in ["queued","hold"]:
+            self._joblist.append(SgeJob(jobname,runtime,initial_status))
+        else:
+            raise SgeError("Initial job status can only be 'queued' or 'hold'")
  
     def get_job_names(self):
         jobnames=[]
         for job in self._joblist:
-            jobnames.append(job._jobname)
+            jobnames.append(job.jobname)
         return jobnames
  
     def get_job_status(self,jobname):
         for job in self._joblist:
-            if job._jobname==jobname:
-                return job._status
+            if job.jobname==jobname:
+                return job.status
         return "absent"
  
-    def tick(ticklength=1):
-        pass
+    def tick(self,ticklength=1):
+        for job in self._joblist:
+            if job.status=="running":
+                job.runtime=job.runtime-ticklength
+            
  
 class TestSgeEmulator(unittest.TestCase):
- 
-    def setUp(self):
-        self.emu=SgeEmulator()
-       
+
     def test_adding_job(self):
         #when you add a job, it should go in the job list, either queued or running
-        self.emu.add_job("test job")
-        self.assertTrue("test job" in self.emu.get_job_names())
-        self.assertTrue(self.emu.get_job_status("test job")
+        emu=SgeEmulator()
+        emu.add_job("test job")
+        self.assertTrue("test job" in emu.get_job_names())
+        self.assertTrue(emu.get_job_status("test job")
                         in ["queued","running"])
+
+    def test_adding_running_job(self):
+        #when you try to add a job that is not queued or on hold, you should get an exception
+        emu=SgeEmulator()
+        self.assertRaises(SgeError,emu.add_job,"test job",100,"running")
  
     def test_job_runs(self):
         #when you add a job to an emulator with exactly one available slot, the job
         #should start running on the next tick
-        emu2=SgeEmulator(1,1)
-        emu2.add_job("test",100)
-        emu2.tick()
-        self.assertTrue(emu2.get_job_status("test")=="running")
+        emu=SgeEmulator(1,1)
+        emu.add_job("test",100)
+        emu.tick()
+        self.assertTrue(emu.get_job_status("test")=="running")
       
 if __name__ == "__main__":
     unittest.main()
